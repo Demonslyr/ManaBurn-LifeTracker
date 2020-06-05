81f3976b93a7e4560316a617250356a9f43fc416
Daniel Murtha <Dr.Murtha@me.com>
2020-06-04 23:16:13 -0500

    group add methods

--- ManaBurnServer/ManaBurnServer/Hubs/GameHub.cs
+++ ManaBurnServer/ManaBurnServer/Hubs/GameHub.cs
@@ -5,10 +5,23 @@ namespace ManaBurnServer.Hubs
 {
     public class GameHub : Hub
     {
-        public async Task SendMessage(string user,string session, string action)
+        public async Task SendMessage(string user,string gameSession, string action)
         {
             // Send message to clients in the session, do some checks to make sure the client belongs to the session they passed before sending
-            await Clients.Group(session).SendAsync("ReceiveMessage", user, action);
+            await Clients.Group(gameSession).SendAsync("ReceiveMessage", user, action);
+        }
+        public async Task AddToGroup(string gameSession)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, gameSession);
+
+            await Clients.Group(gameSession).SendAsync("Send", $"{Context.ConnectionId} has joined the group {gameSession}.");
+        }
+
+        public async Task RemoveFromGroup(string gameSession)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, gameSession);
+
+            await Clients.Group(gameSession).SendAsync("Send", $"{Context.ConnectionId} has left the group {gameSession}.");
         }
     }
 }
