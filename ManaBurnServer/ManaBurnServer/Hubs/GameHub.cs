2ba83e641fcc41d2f6b48cf4eabb6022a2b11fde
Daniel Murtha <Dr.Murtha@me.com>
2020-06-04 23:09:29 -0500

    There's no way this is all that's needed

---
+++ ManaBurnServer/ManaBurnServer/Hubs/GameHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ManaBurnServer.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SendMessage(string user,string session, string action)
+        {
+            // Send message to clients in the session, do some checks to make sure the client belongs to the session they passed before sending
+            await Clients.Group(session).SendAsync("ReceiveMessage", user, action);
+        }
+    }
+}
