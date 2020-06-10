535d912ea5b085d24e8dbb06ce3ddf33a53d6d16
Daniel Murtha <Dr.Murtha@me.com>
2020-06-10 12:52:33 -0500

    stubbing

---
+++ ManaBurnServer/ManaBurnServer/Controllers/GameSessionController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManaBurnServer.Controllers
+{
+    public class GameSessionController
+    {
+    }
+}
