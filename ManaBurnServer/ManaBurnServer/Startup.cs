6b9ff8b7daf0ad5d5e2d08e159aa2b09f7e710cd
Daniel Murtha <Dr.Murtha@me.com>
2020-06-10 02:07:51 -0500

    removing till it works

--- ManaBurnServer/ManaBurnServer/Startup.cs
+++ ManaBurnServer/ManaBurnServer/Startup.cs
@@ -74,7 +74,7 @@ namespace ManaBurnServer
             .AddStackExchangeRedis(o =>
             {
                 o.Configuration.AbortOnConnectFail = false;
-                o.Configuration.ClientName = $"{Environment.EnvironmentName}-{Environment.ApplicationName}";
+               // o.Configuration.ClientName = $"{Environment.EnvironmentName}-{Environment.ApplicationName}";
                 o.Configuration.EndPoints.Add(
                     Configuration.GetSection("Atriarch_Redis_Host").Value, 
                     Configuration.GetSection("Atriarch_Redis_Port").Get<int>()
