49606aa0b0baa3ad61e339f68fb3ed6e91aaaace
Daniel Murtha <Dr.Murtha@me.com>
2020-06-13 19:01:23 -0500

    Swagger configuration

--- ManaBurnServer/ManaBurnServer/Extensions/ManaburnSwaggerUIOptions.cs
+++ ManaBurnServer/ManaBurnServer/Extensions/ManaburnSwaggerUIOptions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.SwaggerUI;
@@ -7,13 +8,15 @@ namespace ManaBurnServer.Extensions
 {
     public sealed class ManaburnSwaggerUIOptions : IConfigureOptions<SwaggerUIOptions>
     {
+        private readonly IWebHostEnvironment _environment;
+
         public ManaburnSwaggerUIOptions(IConfiguration configuration, IWebHostEnvironment environment)
         {
-
+            _environment = environment;
         }
         public void Configure(SwaggerUIOptions options)
         {
-
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", $"{_environment.ApplicationName} V1");
         }
     }
 }
