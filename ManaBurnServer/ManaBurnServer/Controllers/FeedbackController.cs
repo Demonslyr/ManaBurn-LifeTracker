9fad6955d245a02bf0101a8bc3cfe7e39a8e8961
Daniel Murtha <Dr.Murtha@me.com>
2020-06-13 22:59:20 -0500

    feedbackController endpoint update

--- ManaBurnServer/ManaBurnServer/Controllers/FeedbackController.cs
+++ ManaBurnServer/ManaBurnServer/Controllers/FeedbackController.cs
@@ -29,20 +29,25 @@ namespace ManaBurnServer.Controllers
         /// Needs to return a list of Product summaries
         /// </summary>
         [HttpPost]
-        public async Task<ActionResult<IEnumerable<Feedback>>> GetPageOfFeedback(FeedbackRequest feedbackRequest)
+        public async Task<ActionResult> GetPageOfFeedback(FeedbackRequest feedbackRequest)
         {
             try
             {
                 // expecting page number (0 indexed)
                 // expecting number of records per page (size of page)
                 var userId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
-                _logger.LogInformation("Received a request to get product list from user inventory! {userId} {PageNumber} {PageSize}", userId, feedbackRequest.PageNumber, feedbackRequest.PageSize);
-                return Ok(await _feedbackRepository.SelectFeedbackRecordsByPage(feedbackRequest.PageNumber, feedbackRequest.PageSize));
+                _logger.LogInformation("Received a request to get a page of feedback! {userId} {PageNumber} {PageSize}", userId, feedbackRequest.PageNumber, feedbackRequest.PageSize);
+                return Ok(new GenericDataResponseModel<IEnumerable<Feedback>>
+                {
+                    Success = true,
+                    Data = await _feedbackRepository.SelectFeedbackRecordsByPage(feedbackRequest.PageNumber,
+                        feedbackRequest.PageSize)
+                });
             }
             catch (Exception e)
             {
                 // Todo: Add more identifiable information about this failure like userId or the token.
-                _logger.LogError(e, $"Get enduser inventory product exception: {e.Message}.", feedbackRequest);
+                _logger.LogError(e, $"Get feedback page exception: {e.Message}.", feedbackRequest);
                 return Ok(new GenericResponseModel
                 {
                     Errors = e.Message
@@ -56,33 +61,64 @@ namespace ManaBurnServer.Controllers
             try
             {
                 var userId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
-                _logger.LogInformation("Received a request to put product in user inventory!", userId);
-                await _feedbackRepository.CreateFeedbackRecord(feedback, userId);
-                return Ok();
+                _logger.LogInformation("Received a request to put feedback!", userId);
+                return Ok(new GenericDataResponseModel<string>
+                {
+                    Success = true,
+                    Data = await _feedbackRepository.CreateFeedbackRecord(feedback, userId)
+                });
             }
             catch (Exception e)
             {
                 // Todo: Add more identifiable information about this failure like userId or the token.
-                _logger.LogError(e, $"Put enduser inventory product exception: {e.Message}.");
+                _logger.LogError(e, $"Put feedback exception: {e.Message}.");
                 return Ok(new GenericResponseModel
                 {
                     Errors = e.Message
                 });
             }
         }
+        [HttpPatch]
+        public async Task<ActionResult> SetFeedbackStatus(object feedbackStatusChangeRequest)
+        {
+            try
+            {
+                var userId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+                _logger.LogInformation("Received a request to set feedback status!", userId);
+                return Ok(new GenericResponseModel
+                {
+                    Success = false,
+                    //Data = TODO: implement this
+                });
+            }
+            catch (Exception e)
+            {
+                // Todo: Add more identifiable information about this failure like userId or the token.
+                _logger.LogError(e, $"Set feedback status exception: {e.Message}.");
+                return Ok(new GenericResponseModel
+                {
+                    Errors = e.Message
+                });
+            }
+        }
+
         [HttpDelete]
-        public async Task<ActionResult<int>> DeleteProductFromUserInventory(string feedbackRecordId)
+        public async Task<ActionResult> DeleteProductFromUserInventory(string feedbackRecordId)
         {
             try
             {
                 var userId = User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
                 _logger.LogInformation("Received a request to delete feedback!", userId);
-                return await _feedbackRepository.DeleteFeedbackRecord(feedbackRecordId);
+                return Ok(new GenericDataResponseModel<int>
+                {
+                    Success = true,
+                    Data = await _feedbackRepository.DeleteFeedbackRecord(feedbackRecordId)
+                });
             }
             catch (Exception e)
             {
                 // Todo: Add more identifiable information about this failure like userId or the token.
-                _logger.LogError(e, $"Delete enduser inventory product exception: {e.Message}.");
+                _logger.LogError(e, $"Delete feedback exception: {e.Message}.");
                 return Ok(new GenericResponseModel
                 {
                     Errors = e.Message
